4b013cc056ea725938fa7c7c2ece2ee250850d0b
FanOfStars <ramazanovapea@gmail.com>
2025-11-07 18:23:42 +0300

    Добавление конфигурации для навигации

--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,28 @@
 import 'package:flutter/material.dart';
-import '../features/auth/presentation/register_screen.dart';
-import 'features/auth/presentation/login_screen.dart';
-import 'features/ui/main_screen.dart';
+import 'package:go_router/go_router.dart';
+
+import 'core/app_router.dart';
+
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  bool _isAuthScreen = true; // true -> экран входа, false -> экран регистрации
-  bool _isLoggedIn = false; // true -> показываем MainScreen
-
-  void _onLogin(String login, String password) {
-    // здесь могла бы быть логика проверки логина/пароля
-    setState(() {
-      _isLoggedIn = true;
-    });
-  }
-
-  void _onRegister(String login, String password) {
-    // здесь могла бы быть логика регистрации пользователя
-    setState(() {
-      _isLoggedIn = true;
-    });
-  }
-
-  void _switchToRegister() {
-    setState(() {
-      _isAuthScreen = false;
-    });
-  }
-
-  void _switchToLogin() {
-    setState(() {
-      _isAuthScreen = true;
-    });
-  }
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Text Blocks App',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: _isLoggedIn
-          ? const MainScreen()
-          : (_isAuthScreen
-          ? AuthScreen(
-        onLogin: _onLogin,
-        onRegisterTap: _switchToRegister,
-      )
-          : RegisterScreen(
-        onRegister: _onRegister,
-      )),
+    return MaterialApp.router(
+      routerConfig: AppRouter.router,
+      title: 'Text Editor App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.light,
+      debugShowCheckedModeBanner: false,
     );
   }
 }
